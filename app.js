a767cd6a833bd05f1493bdbd7ba062ed5f121f97
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-25 18:37:08 +0100

    add startingMessage  function.
    this will get value from user

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const http = require('http');
 const prompt = require('prompt-sync')({sigint: true});
+
+// functions 
+
+// starting message 
+
+function startingMsg() {
+	console.log(`Hello There!`);
+	let num = prompt(`Enter your value in Euro to convert to whatever you want: `);
+	
+}
